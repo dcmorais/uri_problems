da33dabbbca30c676103d70783c366e0666cb937
Daniel Morais <dcmorais@gmail.com>
2017-08-05 22:53:04 -0300

    first commit

---
+++ beginner/1002.rb
@@ -0,0 +1,7 @@
+pi =  3.14159
+
+R = gets.to_f
+
+A = pi*R*R
+
+puts sprintf('A=%.4f', A)
---
+++ beginner/1006.rb
@@ -0,0 +1,7 @@
+a = gets.to_f
+b = gets.to_f
+c = gets.to_f
+
+MEDIA = (a*2 + b*3 + c*5)/(2+3+5)
+
+puts sprintf('MEDIA = %.1f', MEDIA)
